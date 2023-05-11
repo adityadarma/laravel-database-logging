631a700904d0848d185373b07997a83344b6b598
Aditya Darma <adhit.boys1@gmail.com>
2023-05-12 06:05:45 +0800

    first commit

--- src/LoggingData.php
+++ src/LoggingData.php
@@ -14,19 +14,19 @@ class LoggingData
     }
 
 
-    public static function store(): mixed
+    public static function store(): void
     {
-        if (!auth()->check() || !config('database-logging.enable_logging', true) || request()->method() == 'GET') return;
-
-        $auth = auth()->user();
-        DatabaseLogging::create([
-            'loggable_id' => $auth->getKey(),
-            'loggable_type' => $auth->getMorphClass(),
-            'url' => request()->fullUrl(),
-            'agent' => request()->userAgent(),
-            'ip_address' => request()->ip(),
-            'method' => request()->method(),
-            'data' => json_encode(app(LoggingData::class)->data)
-        ]);
+        if (auth()->check() || config('database-logging.enable_logging', true) || request()->method() != 'GET'){
+            $auth = auth()->user();
+            DatabaseLogging::create([
+                'loggable_id' => $auth->getKey(),
+                'loggable_type' => $auth->getMorphClass(),
+                'url' => request()->fullUrl(),
+                'agent' => request()->userAgent(),
+                'ip_address' => request()->ip(),
+                'method' => request()->method(),
+                'data' => json_encode(app(LoggingData::class)->data)
+            ]);
+        }
     }
 }
