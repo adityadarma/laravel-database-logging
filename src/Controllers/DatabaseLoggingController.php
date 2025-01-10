1d5199631f5dad0aa8c1728ad5e36889093ce509
Aditya Darma <adhit.boys1@gmail.com>
2025-01-10 22:41:13 +0800

    fix controller view

--- src/Controllers/DatabaseLoggingController.php
+++ src/Controllers/DatabaseLoggingController.php
@@ -34,10 +34,10 @@ class DatabaseLoggingController extends Controller
                 $query->where('loggable_id', $exp[1] !== '' ? $exp[1] : null);
             })
             ->when($request->table, function ($query) use ($request) {
-                $query->whereJsonContains('data', [['table' => $request->table]]);
+                $query->whereJsonContains('data', ['table' => $request->table]);
             })
             ->when($request->id, function ($query) use ($request) {
-                $query->whereJsonContains('data', [['id' => (int) $request->id]]);
+                $query->whereJsonContains('data', ['id' => $request->id]);
             })
             ->when($request->date_start, function ($query) use ($request) {
                 $query->where('created_at', '>=', $request->date_start.' 00:00:00');
