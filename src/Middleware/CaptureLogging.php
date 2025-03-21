a280d4f7ce3aadcd3b1c63a23509d191a1234d0f
Aditya Darma <adhit.boys1@gmail.com>
2025-03-21 13:42:04 +0800

    filter error on middleware

--- src/Middleware/CaptureLogging.php
+++ src/Middleware/CaptureLogging.php
@@ -4,9 +4,7 @@ namespace AdityaDarma\LaravelDatabaseLogging\Middleware;
 
 use AdityaDarma\LaravelDatabaseLogging\LoggingData;
 use Closure;
-use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use JsonException;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -24,8 +22,10 @@ class CaptureLogging
 
         $response = $next($request);
 
-        LoggingData::store($request, $response);
-        
+        if ($response->getStatusCode() >= 400) {
+            LoggingData::store($request, $response);
+        }
+
         return $response;
     }
 }
